ed2534c2fe02ac4fddf3b41836e9e26aacd038a5
B. Cedric Cogell <1653832+cogell@users.noreply.github.com>
2026-03-17 15:35:28 -0400

    sshot 1.0.0

--- Casks/sshot.rb
+++ Casks/sshot.rb
@@ -1,6 +1,6 @@
 cask "sshot" do
   version "1.0.0"
-  sha256 "PLACEHOLDER"
+  sha256 "18a06ecc2a04411ac8d1c4dc9ad583cd23be5231eca398142ed9326216060819"
 
   url "https://github.com/cogell/sshot/releases/download/v#{version}/SSHot-#{version}.zip"
   name "SSHot"
